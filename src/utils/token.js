69ff0b99af9291be36a7557ce66547cb04d4eb4d
Takkyou <takkyou@Takkyouui-MacBookPro.local>
2023-05-19 16:17:34 +0900

    isAuth

--- src/utils/token.js
+++ src/utils/token.js
@@ -6,3 +6,16 @@ export const generateToken = (user) => {
   });
   return token;
 };
+
+export const isAuth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer xxxxx
+    const decode = jwt.verify(token, process.env.SECRETE);
+    req.user = decode;
+    next();
+  } else {
+    res.status(401).json({ message: '토큰이 없다' });
+  }
+};
