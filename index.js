49698ad71225b4c493f2f0799e158fa1bc669735
gwanseeum <9v.veu.v@gmail.com>
2023-05-27 21:02:53 +0900

    Fix: cors 에러

--- index.js
+++ index.js
@@ -14,19 +14,25 @@ import { collectionRouter, imgRouter, chatRouter, userRouter } from './src/route
 const app = express();
 
 // Middleware
-app.use(
-  cors({
-    origin: '*',
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-    maxAge: 86400,
-    optionsSuccessStatus: 200,
-  })
-);
+// app.use(
+//   cors({
+//     origin: '*',
+//     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true,
+//     maxAge: 86400,
+//     optionsSuccessStatus: 200,
+//   })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use('/api', collectionRouter);
 app.use('/api', imgRouter);
