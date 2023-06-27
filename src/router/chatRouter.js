9d9c679b92b1f06d49e2e34eb0fe9cdc9a2a4e5b
김지우 <ziuss76@gmail.com>
2023-06-27 20:42:46 +0900

    Fix: isAuth 라우터에 추가

--- src/router/chatRouter.js
+++ src/router/chatRouter.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { chatController } from '../controller/index.js';
+import { isAuth } from '../utils/token.js';
 const chatRouter = Router();
 
-chatRouter.post('/chat', chatController.createChat);
-chatRouter.post('/chat/last', chatController.createLastChat);
+chatRouter.post('/chat', isAuth, chatController.createChat);
+chatRouter.post('/chat/last', isAuth, chatController.createLastChat);
 
 export default chatRouter;
--- src/router/imgRouter.js
+++ src/router/imgRouter.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { imgController } from '../controller/index.js';
+import { isAuth } from '../utils/token.js';
 const imgRouter = Router();
 
-imgRouter.post('/img', imgController.createImg);
+imgRouter.post('/img', isAuth, imgController.createImg);
 
 export default imgRouter;
