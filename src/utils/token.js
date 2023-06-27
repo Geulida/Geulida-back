5491e3f3c3cb8ff81afc64ec7f8da1122504ce39
Takkyou <takelucky777@naver.com>
2023-06-27 20:39:56 +0900

    token error

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import { AppError, CommonError } from '../middlewares/errorHandler.js';
 
 export const generateToken = (user) => {
   const token = jwt.sign({ email: user.email }, process.env.SECRET_KEY, {
@@ -11,11 +12,15 @@ export const isAuth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (authorization) {
-    const token = authorization.slice(7, authorization.length); // Bearer xxxxx
-    const decode = jwt.verify(token, process.env.SECRET_KEY);
-    req.user = decode;
-    next();
+    try {
+      const token = authorization.slice(7, authorization.length);
+      const decode = jwt.verify(token, process.env.SECRET_KEY);
+      req.user = decode;
+      next();
+    } catch (err) {
+      throw new AppError(CommonError.TOKEN_EXPIRED_ERROR, '잘못된 토큰입니다', 500);
+    }
   } else {
-    res.status(401).json({ message: '토큰이 없다' });
+    res.status(401).json({ message: '발급된 토큰이 없습니다' });
   }
 };
