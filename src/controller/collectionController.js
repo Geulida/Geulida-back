8e6c543059785f7d7de5984360c633e2255e8f11
김승환 <prpn97@gmail.com>
2023-06-28 13:58:12 +0900

    Fix : 페이지별 컬렉션 확인시 해당 email에 맞는 컬렉션만 확인

--- src/controller/collectionController.js
+++ src/controller/collectionController.js
@@ -1,7 +1,6 @@
 import CollectionService from '../services/collectionService.js';
 import { AppError, CommonError } from '../middlewares/errorHandler.js';
 
-
 const collectionController = {
   async createCollection(req, res, next) {
     const { color, hexcode, style, summary, url } = req.body;
@@ -22,9 +21,11 @@ const collectionController = {
   async getCollection(req, res, next) {
     const { id } = req.params;
     const email = req.user.email;
+
     try {
       const collectionService = new CollectionService();
       const collection = await collectionService.getCollection(id);
+
       if (!email) {
         throw new AppError(CommonError.UNAUTHORIZED_ACCESS, '로그인 후 이용 가능합니다.', 401);
       }
@@ -44,19 +45,16 @@ const collectionController = {
   async getCollectionsByPage(req, res, next) {
     const { page } = req.query; // /collection?page=2
     const email = req.user.email;
+
+    const collectionsPerPage = 12; // 페이지당 컬렉션 수
+    const offset = (page - 1) * collectionsPerPage; // 오프셋 계산
     try {
+      const collectionService = new CollectionService();
+      const collections = await collectionService.getCollectionsPerPage(collectionsPerPage, offset, email);
+
       if (!email) {
         throw new AppError(CommonError.UNAUTHORIZED_ACCESS, '로그인 후 이용 가능합니다.', 401);
       }
-      if (email !== collection.email) {
-        throw new AppError(CommonError.UNAUTHORIZED_ACCESS, '나의 컬렉션만 조회가 가능합니다.', 401);
-      }
-      const collection = await collectionService.getCollection(id);
-      const collectionService = new CollectionService();
-      const collectionsPerPage = 12; // 페이지당 컬렉션 수
-      const offset = (page - 1) * collectionsPerPage; // 오프셋 계산
-
-      const collections = await collectionService.getCollectionsPerPage(collectionsPerPage, offset);
       if (!collections.length) {
         throw new AppError(CommonError.RESOURCE_NOT_FOUND, '해당 페이지는 존재하지 않습니다.', 404);
       }
@@ -87,7 +85,7 @@ const collectionController = {
         throw new AppError(CommonError.RESOURCE_NOT_FOUND, '없는 컬렉션입니다.', 404);
       }
 
-      res.status(200).json({ message : "삭제되었습니다."});
+      res.status(200).json({ message: '삭제되었습니다.' });
     } catch (error) {
       console.error(error);
       next(error);
--- src/services/collectionService.js
+++ src/services/collectionService.js
@@ -21,20 +21,8 @@ class CollectionService {
         throw error;
       } else {
         throw new AppError(CommonError.UNEXPECTED_ERROR, '컬렉션 생성에 실패했습니다.', 500);
-      }   }
-  }
-
-  async getCollectionsPerPage(collectionsPerPage, offset) {
-    try {
-      const collections = await Collection.find().skip(offset).limit(collectionsPerPage);
-      return collections;
-    } catch (error) {
-      console.error(error);
-      if (error instanceof AppError) {
-        throw error;
-      } else {
-        throw new AppError(CommonError.UNEXPECTED_ERROR, '컬렉션 조회에 실패했습니다.', 500);
-      }}
+      }
+    }
   }
 
   async getCollection(id) {
@@ -43,12 +31,12 @@ class CollectionService {
         // 입력값이 유효한 ObjectId 형식이 아닌 경우 404 에러 반환
         throw new AppError(CommonError.RESOURCE_NOT_FOUND, '존재하지 않는 컬렉션입니다.', 404);
       }
-    
+
       const collection = await Collection.findById(id).exec();
       if (!collection) {
         throw new AppError(CommonError.RESOURCE_NOT_FOUND, '존재하지 않는 컬렉션입니다.', 404);
       }
-    
+
       return collection;
     } catch (error) {
       console.error(error);
@@ -59,7 +47,20 @@ class CollectionService {
       }
     }
   }
-  
+  async getCollectionsPerPage(collectionsPerPage, offset, email) {
+    try {
+      const collections = await Collection.find({ email }).skip(offset).limit(collectionsPerPage);
+
+      return collections;
+    } catch (error) {
+      console.error(error);
+      if (error instanceof AppError) {
+        throw error;
+      } else {
+        throw new AppError(CommonError.UNEXPECTED_ERROR, '컬렉션 조회에 실패했습니다.', 500);
+      }
+    }
+  }
 
   async deleteCollection(id) {
     try {
